4d73ea2b911d6a81027bcada6f62b2d1534b08d9
Daniel Ahn <iamdanahn@gmail.com>
2021-03-24 20:50:55 -0400

    Add questions 1 & 2

--- 03-22-2021.js
+++ 03-22-2021.js
@@ -27,3 +27,34 @@ function deviceNamesSystem(devicenames) {
 	}
 	return newNames;
 }
+
+
+
+
+/*
+ * Complete the 'mergeInBetween' function below.
+ *
+ * The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+ * The function accepts following parameters:
+ *  1. INTEGER_SINGLY_LINKED_LIST list1
+ *  2. INTEGER_SINGLY_LINKED_LIST list2
+ *  3. INTEGER a
+ *  4. INTEGER b
+ */
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+
+function mergeInBetween(list1, list2, a, b) {
+    // Write your code here
+    
+    return list1.slice(0, a) + list2 + list1.slice(b, list1.length)
+
+}
